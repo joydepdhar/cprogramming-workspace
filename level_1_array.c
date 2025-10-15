4a1e3644900c51d3d44d7b0a3a5c7d862589cde3
joydepdhar <joydepdhar138@gmail.com>
2025-10-15 14:49:35 +0600

    level1ofarray

--- level_1_array.c
+++ level_1_array.c
@@ -172,22 +172,22 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int num[5]={1,2,3,4,5};
-    int n=5;
-    int flag=0;
-    for(int i=0;i<n-1;i++){
-        if(num[i]>num[i+1]){
-            flag=1;
-            break;
-        }
-    }
-    if(flag==1){
-        printf("not sort");
-    }else{
-        printf("Sort");
-    }
-    return 0;
-}
+// int main() {
+//     int num[5]={1,2,3,4,5};
+//     int n=5;
+//     int flag=0;
+//     for(int i=0;i<n-1;i++){
+//         if(num[i]>num[i+1]){
+//             flag=1;
+//             break;
+//         }
+//     }
+//     if(flag==1){
+//         printf("not sort");
+//     }else{
+//         printf("Sort");
+//     }
+//     return 0;
+// }
