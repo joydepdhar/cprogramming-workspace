e4e988aa8ab049131ef54692b56b2297ae7649c7
joydepdhar <joydepdhar138@gmail.com>
2025-10-19 00:34:42 +0600

    okay new problem

---
+++ removingelement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n-1];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int idx;
+    scanf("%d",&idx);
+    return 0;
+    for(int i=idx;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n--;
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
