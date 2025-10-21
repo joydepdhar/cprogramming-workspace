20f8b26265cb7955605e067e6824c28ed4a674bf
joydepdhar <joydepdhar138@gmail.com>
2025-10-21 23:41:01 +0600

    concat

---
+++ stringOperations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char a[101],b[101];
+
+    scanf("%s%s",a,b);
+    int sza=strlen(a);
+    int szb=strlen(b);
+    for(int i=0;i<szb;i++){
+        a[sza+i]=b[i];
+    }
+    printf("%s ",a);
+    return 0;
+}
