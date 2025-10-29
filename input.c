d1639af853c80330e09f13e8467177fc3c3ce4fe
joydepdhar <joydepdhar138@gmail.com>
2025-10-29 23:29:00 +0600

    inputoutput

---
+++ input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d",&x);
+    char a;
+    scanf("%c",&a);
+    float b;
+    scanf("%f",&b);
+    
+    printf("%d",x);
+    printf("%c",a);
+    printf("%f",b);
+    return 0;
+}
