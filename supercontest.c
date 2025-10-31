0c3ddca1308b4dc8f653dfc229e28e5bbfe66991
joydepdhar <joydepdhar138@gmail.com>
2025-10-31 22:19:25 +0600

    more

--- supercontest.c
+++ supercontest.c
@@ -7,8 +7,24 @@ int main() {
     // int _a_=53;
     // int a3b2=7;
     // printf("%c",_a+_a_+a3b2); //ascii value print korbe
-    long long int a=1e10;
-    int b=100;
-    int c=a/b;
-    return 0;
+    // long long int a=1e10;
+    // int b=100;
+    // int c=a/b;
+    // return 0;
+    // int a=6,b=4;
+    // double c=(double)a/b;
+    // printf("%lf",a/b);
+    // printf("\n");
+    // printf("%lf",c);
+    // int ans=6*7/3*2;
+    // printf("%d",ans);
+    // double a=2.5,b=1.5;
+    // printf("%lf",a % b);
+    int x=5;
+    x++;
+    --x;
+    printf("%d",x++);
+    printf("%d",x);
+    printf("%d",--x);
+
 }
