78d3a9eb8b8a56e28764bb4901beb08e68f2027a
joydepdhar <joydepdhar138@gmail.com>
2025-11-01 22:34:26 +0600

    selectionsort problem solve

--- selectionsort.c
+++ selectionsort.c
@@ -7,10 +7,8 @@ int main() {
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    // int key,flag=0;
-    // scanf("%d",&key);
     for(int i=0;i<n-1;i++){
-        for(int j=1;j<n;j++){
+        for(int j=i+1;j<n;j++){
             if(arr[i]>arr[j]){
                 int temp=arr[i];
                 arr[i]=arr[j];
