c143bd355b65af20e48fce98e8957f9a9fd12d34
joydepdhar <joydepdhar138@gmail.com>
2025-11-03 00:16:32 +0600

    super

---
+++ varaiableandscope.c
@@ -0,0 +1,12 @@
+// // // // 
+// // // function-
+// // // 1. Built in function
+// // // -strlen();
+// // // -strcmp(val,val);
+// // //use full math function;
+// // // 1. ceil function 
+// // 2. floor function
+// // 3. sqrt function
+// 4. abs function
+// 5. pow
+// // // 2. user defined function
