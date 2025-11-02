6c36f0845d732a9ff751727f09f36d62ff1c8d70
joydepdhar <joydepdhar138@gmail.com>
2025-11-02 23:58:36 +0600

    f

--- exploringfunction.c
+++ exploringfunction.c
@@ -25,15 +25,27 @@
 //     return 0;
 // }
 // 
-// 3. no return + parameters
+// // 3. no return + parameters
+// #include <stdio.h>
+// void sum(int a, int b){
+//     int ans=a+b;
+//     printf("%d",ans);
+// }
+// int main() {
+//     int a=40,b=20;
+//     sum(40,20);
+//     return 0;
+// }
+// 4. no return + no parameters
 #include <stdio.h>
-void sum(int a, int b){
-    int ans=a+b;
-    printf("%d",ans);
+int sum(){
+    int a,b;
+    int sum;
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("%d",sum);
 }
 int main() {
-    int a=40,b=20;
-    sum(40,20);
+    sum();
     return 0;
 }
-// 4. no return + no parameters
