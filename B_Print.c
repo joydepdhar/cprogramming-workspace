bcd3b586beac9dcd9b4d8fc53ec3540b62dbd84d
joydepdhar <joydepdhar138@gmail.com>
2025-11-06 21:50:00 +0600

    wrong solve

--- B_Print.c
+++ B_Print.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-void print(long long int x){
-    for(int i=1;i<=x;i++){
-        printf("%d ",i);
+
+void print(long long int x) {
+    for (long long int i = 1; i <= x; i++) {
+        printf("%lld ", i);
     }
 }
+
 int main() {
     long long int x;
-    scanf("%lld",&x);
+    scanf("%lld", &x);
     print(x);
     return 0;
 }
